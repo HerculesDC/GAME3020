2ebb34eb87b4e9e255ec55bc8ea911fdf4dfc08c
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2019-09-08 00:52:28 -0400

    First commit, with the tractors-and-chain model in a preliminary stage

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] private float maxVel;
+    [SerializeField] private float impulse;
+    [SerializeField] private float torque;
+
+    private Rigidbody m_rb;
+
+    void Awake() {
+        m_rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_rb) {
+            //m_rb.AddTorque(this.gameObject.transform.up * torque);
+            if (m_rb.velocity.sqrMagnitude < maxVel)
+                m_rb.AddForce(this.gameObject.transform.forward * impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPositioning.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositioning : MonoBehaviour
+{
+    //The idea is to create a point towards which the camera can look at
+    //opted to look for Game Objects instead of Transforms, in case I need some other property from them
+    [SerializeField] private GameObject m_gLeft;
+    [SerializeField] private GameObject m_gRight;
+    [SerializeField] private Vector3 m_vFacing; //***FOR READING PURPOSES ONLY***
+
+    void Awake() {
+        m_vFacing = Vector3.zero;
+        if (!m_gLeft) m_gLeft = GameObject.Find("Left");
+        if (!m_gRight) m_gRight = GameObject.Find("Right");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!m_gLeft) m_gLeft = GameObject.Find("Left");
+        if (!m_gRight) m_gRight = GameObject.Find("Right");
+        //StartCoroutine(DelayedPrint());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_gLeft && m_gRight)
+        {
+            this.gameObject.transform.position = (m_gLeft.transform.position + m_gRight.transform.position) / 2;
+            SetFacing();
+            this.gameObject.transform.LookAt(this.gameObject.transform.position + m_vFacing);
+        }
+        
+    }
+
+    void SetFacing() {
+
+        //THIS LOOKS UGLY AND INEFFICIENT... (but it works, so I'll leave it for now)
+        float tempX = m_gRight.transform.position.x - m_gLeft.transform.position.x;
+        float tempZ = m_gRight.transform.position.z - m_gLeft.transform.position.z;
+
+        float angle = Mathf.Atan2(tempZ, tempX);
+
+        tempX = Mathf.Cos(angle);
+        tempZ = Mathf.Sin(angle);
+        m_vFacing = new Vector3(-tempZ, 0, tempX);
+        m_vFacing.Normalize();
+    }
+    /*
+    IEnumerator DelayedPrint() {
+        while (true) {
+            yield return new WaitForSeconds(2.0f);
+            Debug.Log(m_vFacing);
+        }
+    }
+    //*/
+}
