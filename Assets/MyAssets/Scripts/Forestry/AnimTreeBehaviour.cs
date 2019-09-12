9729d95b873a86fe50935ce23390c193b2b31d99
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2019-09-11 20:58:03 -0400

    Tried two kinds of trees, none worked

---
+++ Assets/MyAssets/Scripts/Forestry/AnimTreeBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTreeBehaviour : MonoBehaviour
+{
+    [SerializeField] private string m_sChain;
+    [SerializeField] private string m_sTrigger;
+
+    private TreeCollision m_tcTree;
+    private bool m_bFallen;
+
+    private Animator m_anim = null;
+    
+    void Awake() {
+
+        m_anim = this.gameObject.GetComponent<Animator>();
+
+        m_tcTree = this.gameObject.GetComponentInChildren<TreeCollision>();
+        m_bFallen = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_bFallen) {
+            if (m_tcTree.disturbed) {
+                m_anim.SetTrigger(m_sTrigger);
+                m_bFallen = true;
+            }
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/Forestry/TreeCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeCollision : MonoBehaviour
+{
+    private bool m_bDisturbed;
+    public bool disturbed { get { return m_bDisturbed; } }
+
+    void Awake() {
+        m_bDisturbed = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) {
+        m_bDisturbed = true;
+    }
+}
