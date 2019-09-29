bf0ccf9f4d6d4b6d045561a6e688bb842160f6ff
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2019-09-29 15:58:57 -0400

    Major Breakthrough in AI detection. More work follows

--- Assets/MyAssets/Scripts/AI/AIPlay.cs
+++ Assets/MyAssets/Scripts/AI/AIPlay.cs
@@ -9,14 +9,17 @@ public class AIPlay : MonoBehaviour
 {
     [SerializeField] private float m_fDetectionRange;
     [SerializeField] private GameObject[] m_gPlayer;
-    [SerializeField] private GameObject sphere;
-    [SerializeField] private GameObject sphere2;
+    [SerializeField] private GameObject sphere; //DEBUG
     [SerializeField] private Vector3[] m_vDifferences;
     [SerializeField] private float[] m_fDistances;
+
+    private LineRenderer m_lr = null; //DEBUG
 
     void Awake() {
         m_vDifferences = new Vector3[m_gPlayer.Length];
         m_fDistances = new float[m_gPlayer.Length];
+        this.gameObject.AddComponent<LineRenderer>(); //DEBUG
+        m_lr = this.gameObject.GetComponent<LineRenderer>(); //DEBUG
     }
 
     // Start is called before the first frame update
@@ -28,10 +31,19 @@ public class AIPlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {  
-        sphere.transform.localScale = Vector3.one * m_fDetectionRange;
         UpdatePositions();
-        DrawSecondSphere(SelectTarget());
         Follow(ChooseTarget());
+        
+        //THIS IS DEBUG CODE
+        //The detection range is returning only half a radius, for whatever reason
+        sphere.transform.localScale = Vector3.one * 2 * m_fDetectionRange;
+
+        if (ChooseTarget()) {
+            m_lr.enabled = true;
+            m_lr.SetPosition(0, this.gameObject.transform.position);
+            m_lr.SetPosition(1, ChooseTarget().position);
+        }
+        else m_lr.enabled = false;
     }
 
     /***THESE FUNCTIONS ASSUME THE EXISTENCE OF 2 TRUCKS, ALWAYS***/
@@ -40,23 +52,16 @@ public class AIPlay : MonoBehaviour
         ushort counter = 0;
         foreach (GameObject player in m_gPlayer){
             //there should be an easier way
-            m_vDifferences[counter] = this.gameObject.transform.position - this.gameObject.transform.position;
+            m_vDifferences[counter] = player.transform.position - this.gameObject.transform.position;
             m_fDistances[counter] = m_vDifferences[counter].magnitude;
             counter++;
         }
     }
 
     Transform ChooseTarget() {
-        return null;
-    }
 
-    float SelectTarget() {
-
-        return m_fDistances[0] < m_fDistances[1] ? m_fDistances[0]: m_fDistances[1];
-    }
-
-    void DrawSecondSphere(float radius) {
-        sphere2.transform.localScale = radius < m_fDetectionRange ? Vector3.one * radius: Vector3.zero;
+        if (m_fDistances[0] > m_fDetectionRange && m_fDistances[1] > m_fDetectionRange) return null;
+        return m_fDistances[0] < m_fDistances[1] ? m_gPlayer[0].transform: m_gPlayer[1].transform;
     }
 
     void Follow(Transform t) {
